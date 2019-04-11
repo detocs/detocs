e33d832be55b46c4b5acdafb991c7317766e1245
Adrian Mullings <adralemechk@gmail.com>
2019-04-11 01:37:07 -0400

    Game models

---
+++ models/game.ts
@@ -0,0 +1,8 @@
+export default class Game {
+  constructor(
+    public id: string,
+    public name: string,
+    public shortNames: string[],
+    public hashtags: string[],
+  ) {}
+};
---
+++ models/games.ts
@@ -0,0 +1,17 @@
+import Game from './game';
+
+export default [
+  new Game(
+    'unist',
+    'Under Night In-Birth Exe:Late[st]',
+    [
+      'Under Night In-Birth',
+      'Under Night',
+      'UNIst',
+    ],
+    [
+      'UNIst',
+      'inbirth',
+    ],
+  ),
+];
