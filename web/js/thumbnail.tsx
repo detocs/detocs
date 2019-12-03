c68a658e3b51d3cf76b39923471d69a185879b32
Adrian Mullings <adralemechk@gmail.com>
2019-12-03 00:07:41 -0500

    Fix thumbnails in Chrome
    Replace object elements when changing the source to work around this
    Chromium bug: https://bugs.chromium.org/p/chromium/issues/detail?id=589109

--- web/js/thumbnail.tsx
+++ web/js/thumbnail.tsx
@@ -1,10 +1,14 @@
 import { h, VNode, FunctionalComponent } from 'preact';
 
 export const Thumbnail: FunctionalComponent<{ src: string | null }> =
-({ src }): VNode => (
-  <object
-    data={src || ''}
-    class="thumbnail">
-    <div class="thumbnail-placeholder" />
-  </object>
-);
+({ src }): VNode => {
+  const data = src || '';
+  return (
+    <object
+      key={data} // Chrome doesn't update object elements when you change the data attribute
+      data={data}
+      class="thumbnail">
+      <div class="thumbnail-placeholder" />
+    </object>
+  );
+};
