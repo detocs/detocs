c06ae3baae7d4a3cde2ad2e42d833d2d54097c5d
Adrian Mullings <adralemechk@gmail.com>
2020-04-24 00:54:49 -0400

    Thumbnail: Only play videos on hover/focus

--- web/js/thumbnail.tsx
+++ web/js/thumbnail.tsx
@@ -1,4 +1,5 @@
 import { h, VNode, FunctionalComponent } from 'preact';
+import { useRef } from 'preact/hooks';
 
 import { MediaFile } from '../../models/media';
 
@@ -9,15 +10,23 @@ interface ThumbnailProps {
 export const Thumbnail: FunctionalComponent<ThumbnailProps> = ({ media }): VNode => {
   const data = media?.url || '';
   // TODO: Only play video when focused/hovered
+  const videoRef = useRef<HTMLVideoElement>();
+  const playVideo = (): void => { videoRef.current?.play(); };
+  const pauseVideo = (): void => { videoRef.current?.pause(); };
   return (
     media?.type === 'video' ?
       <video
+        ref={videoRef}
         src={data}
         class="thumbnail"
         muted={true}
         controls={false}
-        autoPlay={true}
+        autoPlay={false}
         loop={true}
+        onMouseEnter={playVideo}
+        onFocus={playVideo}
+        onMouseLeave={pauseVideo}
+        onBlur={pauseVideo}
       >
       </video> :
       <object
