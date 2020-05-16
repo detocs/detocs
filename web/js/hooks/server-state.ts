37d1a053f18837328a13cbde992621131515a494
Adrian Mullings <adralemechk@gmail.com>
2020-05-16 04:25:11 -0400

    useServerState: Use ReconnectingWebSocket

--- web/js/hooks/server-state.ts
+++ web/js/hooks/server-state.ts
@@ -1,9 +1,14 @@
 import { useState, useEffect, StateUpdater } from 'preact/hooks';
+import ReconnectingWebSocket from 'reconnecting-websocket';
 
 export function useServerState<T>(endpoint: URL, initialState: T): [ T, StateUpdater<T> ] {
   const [ state, updateState ] = useState(initialState);
   useEffect(() => {
-    const ws = new WebSocket(endpoint.href);
+    const ws = new ReconnectingWebSocket(endpoint.href, [], {
+      maxReconnectionDelay: 60 * 1000,
+      reconnectionDelayGrowFactor: 2,
+      maxRetries: 40,
+    });
     ws.onmessage = (ev: MessageEvent) => {
       const newState = JSON.parse(ev.data) as T;
       updateState(newState);
