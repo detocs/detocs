f2c3d0ea3fc11838ca353ebc72a2f98830a7526c
Adrian Mullings <adralemechk@gmail.com>
2020-07-19 02:07:01 -0400

    Thumbnail: Show video duration to differentiate from image thumbnails

--- web/js/thumbnail.tsx
+++ web/js/thumbnail.tsx
@@ -1,24 +1,43 @@
 import { h, VNode, FunctionalComponent } from 'preact';
 import { useRef } from 'preact/hooks';
 
-import { MediaFile } from '@models/media';
+import { MediaFile, VideoFile, ImageFile } from '@models/media';
+import { fromMillis } from '@util/timestamp';
 
 interface ThumbnailProps {
   media?: MediaFile | null;
 }
 
-export const Thumbnail: FunctionalComponent<ThumbnailProps> = ({ media }): VNode => {
-  const data = media?.url || '';
-  // TODO: Only play video when focused/hovered
+interface VideoThumbnailProps extends ThumbnailProps {
+  media: VideoFile;
+}
+
+interface ImageThumbnailProps extends ThumbnailProps {
+  media?: ImageFile | null;
+}
+
+export const Thumbnail: FunctionalComponent<ThumbnailProps> = (props): VNode => {
+  switch (props.media?.type) {
+    case 'video':
+      return <VideoThumbnail {...props as VideoThumbnailProps} />;
+    case 'image':
+      return <ImageThumbnail {...props as ImageThumbnailProps} />;
+    default:
+      return <ImageThumbnail media={null} />;
+  }
+};
+
+const VideoThumbnail: FunctionalComponent<VideoThumbnailProps> = ({ media }): VNode => {
   const videoRef = useRef<HTMLVideoElement>();
   const playVideo = (): void => { videoRef.current?.play(); };
   const pauseVideo = (): void => { videoRef.current?.pause(); };
+  // TODO: Show current playback progress?
   return (
-    media?.type === 'video' ?
+    <div className="thumbnail">
       <video
         ref={videoRef}
-        src={data}
-        class="thumbnail"
+        src={media.url}
+        class="thumbnail__media"
         muted={true}
         controls={false}
         {...{'disablePictureInPicture': true}}
@@ -29,13 +48,25 @@ export const Thumbnail: FunctionalComponent<ThumbnailProps> = ({ media }): VNode
         onMouseLeave={pauseVideo}
         onBlur={pauseVideo}
       >
-      </video> :
+      </video>
+      <div className="thumbnail__metadata">
+        {fromMillis(media.durationMs).slice(0, -4)}
+      </div>
+    </div>
+  );
+};
+
+const ImageThumbnail: FunctionalComponent<ImageThumbnailProps> = ({ media }): VNode => {
+  const data = media?.url || '';
+  return (
+    <div className="thumbnail">
       <object
         key={data} // Chrome doesn't update object elements when you change the data attribute
         data={data}
-        class="thumbnail"
+        class="thumbnail__media"
       >
-        <div class="thumbnail-placeholder" />
+        <div class="thumbnail__placeholder" />
       </object>
+    </div>
   );
 };
