4ab469974e47034294402b1d3d740663e8c162a6
Adrian Mullings <adralemechk@gmail.com>
2020-08-11 02:50:37 -0400

    Add PostCSS

---
+++ webcss.js
@@ -0,0 +1,30 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs').promises;
+const path = require('path');
+const postCss = require('postcss');
+const sass = require('sass');
+
+const inFile = 'src/web/styles/detocs.scss';
+const sassOut = inFile + '.css';
+const outFile = 'build/public/detocs.css';
+
+const sassResult = sass.renderSync({
+  file: inFile,
+  outFile: sassOut,
+  includePaths: [ 'node_modules/', 'src/web/styles' ],
+  sourceMap: true,
+});
+
+postCss([])
+  .process(sassResult.css, {
+    from: sassOut,
+    to: outFile,
+    map: { prev: sassResult.map.toString('utf8') },
+  })
+  .then(async result => {
+    await fs.mkdir(path.dirname(outFile), { recursive: true });
+    return Promise.all([
+      fs.writeFile(outFile, result.css),
+      fs.writeFile(outFile + '.map', result.map),
+    ]);
+  });
