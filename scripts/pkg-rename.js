0741ecde9db4e47e947afd774825df3a156a96a4
Adrian Mullings <adralemechk@gmail.com>
2021-03-08 19:08:03 -0500

    Fix server binary file renaming
    Adjusted regex so it only matches unrenamed files.

--- scripts/pkg-rename.js
+++ scripts/pkg-rename.js
@@ -3,11 +3,9 @@ const path = require('path');
 const { name, version } = require('../package.json');
 
 const outputDir = 'dist/server';
-const originalFilesRegex = new RegExp(`${name}-([\\w.]+)`);
+const originalFilesRegex = new RegExp(`${name}-([\\w.]+)$`);
 const files = fs.readdirSync(outputDir);
-console.log(files);
 const originalFiles = files.filter(file => originalFilesRegex.test(file));
-console.log(originalFiles);
 
 originalFiles.map(filename => {
   const newFilename = filename.replace(originalFilesRegex, `${name}-server-${version}_$1`);
